519a2a26258d112066bbea3fc5c45157504e133b
Steffen Mecke <stm2@users.sf.net>
2025-08-24 14:43:21 +0200

    fixed page-assets-plugin

--- .eleventy.js
+++ .eleventy.js
@@ -12,9 +12,12 @@ module.exports = function (eleventyConfig) {
 
   // https://github.com/victornpb/eleventy-plugin-page-assets
   eleventyConfig.addPlugin(pageAssetsPlugin, {
-      mode: "parse",
-	  assetsMatching: "*.png|*.PNG|*.jpg|*.JPG|*.gif|*.GIF",
-      postsMatching: "**/*.md",
+    mode: "directory",
+    assetsMatching: "*.png|*.PNG|*.jpg|*.JPG|*.gif|*.GIF|*.cr|*.nr|*.txt",
+    postsMatching: "**/*.md",
+    hashAssets: false,
+    recursive: true,
+    silent: true,
   });
 
   eleventyConfig.addFilter('dateIso', date => {
